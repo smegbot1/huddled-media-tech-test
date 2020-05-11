0e7c33107cf7a90cfc2b00914cd7380b416dbd61
Daniel Cruickshanks <dankom@live.de>
2020-05-11 17:01:37 +0100

    Deploying app.

---
+++ build/precache-manifest.059d3664d573f922953706509d5aadc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f95f68a5d1c829fde59502a604e642c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df62ee1f95ecf80a79a8",
+    "url": "/static/css/main.8418830a.chunk.css"
+  },
+  {
+    "revision": "88e6b11221991ddf42f8",
+    "url": "/static/js/2.79b4c5b5.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.79b4c5b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df62ee1f95ecf80a79a8",
+    "url": "/static/js/main.47eeae75.chunk.js"
+  },
+  {
+    "revision": "970a759368efb8d0f858",
+    "url": "/static/js/runtime-main.55f7f9c9.js"
+  }
+]);
